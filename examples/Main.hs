8e5ef8d99e5520bd6500ca52035fbb5f73e96f7f
Pavel Krajcevski <Krajcevski@gmail.com>
2016-12-13 00:46:15 -0800

    Add render fn

--- examples/Main.hs
+++ examples/Main.hs
@@ -4,7 +4,17 @@ module Main where
 import Yoga as Yoga
 --------------------------------------------------------------------------------
 
+renderIntFn :: (Float, Float) -> (Float, Float) -> Int -> IO String
+renderIntFn _ _ x = do
+  print x
+  return $ concat ["Node ", show x]
+
+renderStringFn :: (Float, Float) -> (Float, Float) -> String -> IO ()
+renderStringFn _ _ str = putStrLn str
+
 main :: IO ()
 main = do
-  let node = mkNode "Node A: "
-  putStrLn "I did it!"
+  let node = mkNode 0
+  a' <- render node renderIntFn
+  _ <- render a' renderStringFn
+  return ()
--- lib/Yoga.hs
+++ lib/Yoga.hs
@@ -1,19 +1,27 @@
 module Yoga (
   -- Initialization
-  Layout, mkNode, mkContainer,
+  Layout, payload, children, mkNode, mkContainer,
 
   -- Style attributes
   Direction(..), FlexDirection(..), Justify(..), Align(..),
   PositionType(..), Overflow(..), Edge(..),
 
+  -- Rendering
+  render,
+
 ) where
 --------------------------------------------------------------------------------
 import Bindings.Yoga
+import Bindings.Yoga.Enums
 
 import Data.Foldable
-import Data.Traversable
+import Data.Traversable()
 
+import Foreign.C.Types (CFloat)
 import Foreign.ForeignPtr
+
+import Numeric.IEEE
+
 import System.IO.Unsafe
 --------------------------------------------------------------------------------
 
@@ -28,8 +36,8 @@ instance Functor Layout where
 
 instance Foldable Layout where
   foldMap f (Layout cs x _) = f x `mappend` (foldMap (foldMap f) cs)
-  foldl f init (Layout cs x _) = foldl (foldl f) (f init x) cs
-  foldr f init (Layout cs x _) = f x $ foldr (flip $ foldr f) init cs
+  foldl f z (Layout cs x _) = foldl (foldl f) (f z x) cs
+  foldr f z (Layout cs x _) = f x $ foldr (flip $ foldr f) z cs
 
 instance Traversable Layout where
   traverse f (Layout cs x ptr) =
@@ -90,4 +98,29 @@ mkNode x = unsafePerformIO $ do
 mkContainer :: a -> [Layout a] -> Layout a
 mkContainer x cs = unsafePerformIO $ do
   ptr <- c'YGNodeNew
+  forM_ (zip cs [0..]) $ \(child, idx) ->
+    withForeignPtr (internalPtr child) $ \childPtr ->
+    c'YGNodeInsertChild ptr childPtr idx
   Layout cs x <$> newForeignPtr p'YGNodeFree ptr
+
+type RenderFn m a b = (Float, Float) -> (Float, Float) -> a -> m b
+
+renderTree :: Monad m => Layout a -> RenderFn m a b -> m (Layout b)
+renderTree (Layout cs x ptr) f = do
+  let cf = realToFrac :: CFloat -> Float
+  let getLayout lytFn = return $ cf $ unsafePerformIO $ withForeignPtr ptr lytFn
+  left <- getLayout c'YGNodeLayoutGetLeft
+  top <- getLayout c'YGNodeLayoutGetTop
+  width <- getLayout c'YGNodeLayoutGetWidth
+  height <- getLayout c'YGNodeLayoutGetHeight
+  Layout
+    <$> mapM (flip renderTree f) cs
+    <*> f (left, top) (width, height) x
+    <*> pure ptr
+
+render :: Monad m => Layout a -> RenderFn m a b -> m (Layout b)
+render lyt@(Layout _ _ ptr) f = do
+  _ <- return $ unsafePerformIO $ withForeignPtr ptr $ \cptr ->
+        let n = (nan :: CFloat)
+        in c'YGNodeCalculateLayout cptr n n c'YGDirectionLTR
+  renderTree lyt f     
