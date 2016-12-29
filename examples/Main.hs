bcd732417fc2b7fece2706d6e0479e11e9e6cb3d
Pavel Krajcevski <Krajcevski@gmail.com>
2016-12-29 15:27:18 -0500

    First draft of Haskell's Yoga API

--- examples/Main.hs
+++ examples/Main.hs
@@ -5,16 +5,13 @@ import qualified Yoga as Yoga
 --------------------------------------------------------------------------------
 
 renderIntFn :: (Float, Float) -> (Float, Float) -> Int -> IO String
-renderIntFn _ _ x = do
-  print x
-  return $ concat ["Node ", show x]
-
-renderStringFn :: (Float, Float) -> (Float, Float) -> String -> IO ()
-renderStringFn _ _ str = putStrLn str
+renderIntFn (left, top) _ x = do
+  putStrLn $ concat ["Node ", show x, ": (", show left, ", ", show top, ")"]
+  return ""
 
 main :: IO ()
 main = do
-  let node = Yoga.node 100.0 100.0 0
-  a' <- Yoga.render node renderIntFn
-  _ <- Yoga.render a' renderStringFn
+  let cs = [Yoga.exact x 100.0 y | x <- [100.0,200.0..400.0], y <- [1..]]
+  let node = Yoga.hbox (Yoga.startToEnd cs) 0
+  _ <- Yoga.render node renderIntFn
   return ()
--- lib/Yoga.hs
+++ lib/Yoga.hs
@@ -1,10 +1,22 @@
 module Yoga (
   -- Initialization
-  Layout, payload, children, node, container, rightToLeftContainer,
+  Layout,
 
-  -- Style attributes
-  Direction(..), Justify(..), Align(..), PositionType(..), Overflow(..),
+  -- Children layouts
+  startToEnd, endToStart, centered, spaceBetween, spaceAround, wrapped,
+  
+  -- Containers
+  hbox, vbox,
+  hboxLeftToRight, hboxRightToLeft,
+  vboxTopToBottom, vboxBottomToTop,
+
+  -- Leaf nodes
+  Size(..),
+  shrinkable, growable, exact,
+
+  -- Attributes
   Edge(..),
+  stretched, marginSetWith, paddingSetWith,
 
   -- Rendering
   render, foldRender,
@@ -17,10 +29,8 @@ import Bindings.Yoga.Enums
 import Data.Foldable
 import Data.Traversable()
 
-import Foreign.C.Types (CFloat)
+import Foreign.C.Types (CFloat, CInt)
 import Foreign.ForeignPtr
-
-import GHC.Ptr (Ptr)
 
 import Numeric.IEEE
 
@@ -28,57 +38,20 @@ import System.IO.Unsafe
 --------------------------------------------------------------------------------
 
 type NativeNode = ForeignPtr C'YGNode
-type NativeNodePtr = Ptr C'YGNode
-data Layout a =
-  Layout { payload :: a,
-           children :: [Layout a],
-           internalPtr :: NativeNode
-         }
+data Layout a
+  = Container { _payload :: a,
+                _children :: [Layout a],
+                internalPtr :: NativeNode }
+  | Leaf { _payload :: a,
+           internalPtr :: NativeNode }
 
-type RenderFn m a b = (Float, Float) -> (Float, Float) -> a -> m b
-
-instance Functor Layout where
-  fmap f (Layout x cs ptr) = Layout (f x) (fmap (fmap f) cs) ptr
-
-instance Foldable Layout where
-  foldMap f (Layout x cs _) = f x `mappend` (foldMap (foldMap f) cs)
-  foldl f z (Layout x cs _) = foldl (foldl f) (f z x) cs
-  foldr f z (Layout x cs _) = f x $ foldr (flip $ foldr f) z cs
-
-instance Traversable Layout where
-  traverse f (Layout x cs ptr) =
-    Layout <$> f x <*> (sequenceA $ traverse f <$> cs) <*> pure ptr
-  sequenceA (Layout x cs ptr) =
-    Layout <$> x <*> sequenceA (sequenceA <$> cs) <*> pure ptr
-
-data Direction
-  = Column
-  | ColumnReverse
-  | Row
-  | RowReverse
-
-data Justify
-  = Justify'FlexStart
-  | Justify'Center
-  | Justify'FlexEnd
-  | Justify'SpaceBetween
-  | Justify'SpaceAround
-
-data Align
-  = Align'Auto
-  | Align'FlexStart
-  | Align'Center
-  | Align'FlexEnd
-  | Align'Stretch
-
-data PositionType
-  = Relative
-  | Absolute
-
-data Overflow
-  = Visible
-  | Hidden
-  | Scroll
+data Children a
+  = StartToEnd [Layout a]
+  | EndToStart [Layout a]
+  | Centered [Layout a]
+  | SpaceBetween [Layout a]
+  | SpaceAround [Layout a]
+  | Wrap (Children a)
 
 data Edge
   = Edge'Left
@@ -90,31 +63,32 @@ data Edge
   | Edge'Horizontal
   | Edge'Vertical
   | Edge'All
+  deriving (Read, Show, Eq, Ord, Enum, Bounded)
 
-node :: Float -> Float -> a -> Layout a
-node width height x = unsafePerformIO $ do
-  ptr <- c'YGNodeNew
-  c'YGNodeStyleSetWidth ptr (realToFrac width)
-  c'YGNodeStyleSetHeight ptr (realToFrac height)
-  Layout x [] <$> newForeignPtr p'YGNodeFree ptr
+type RenderFn m a b = (Float, Float) -> (Float, Float) -> a -> m b
 
-addChildrenToContainer :: NativeNodePtr -> a -> [Layout a] -> IO (Layout a)
-addChildrenToContainer ptr x cs = do
-  forM_ (zip cs [0..]) $ \(child, idx) ->
-    withForeignPtr (internalPtr child) $ \childPtr ->
-    c'YGNodeInsertChild ptr childPtr idx
-  Layout x cs <$> newForeignPtr p'YGNodeFree ptr
+instance Functor Layout where
+  fmap f (Container x cs ptr) = Container (f x) (fmap (fmap f) cs) ptr
+  fmap f (Leaf x ptr) = Leaf (f x) ptr
 
-container :: a -> [Layout a] -> Layout a
-container x cs = unsafePerformIO $ do
-  ptr <- c'YGNodeNew
-  addChildrenToContainer ptr x cs
+instance Foldable Layout where
+  foldMap f (Container x cs _) = f x `mappend` (foldMap (foldMap f) cs)
+  foldMap f (Leaf x _) = f x
 
-rightToLeftContainer :: a -> [Layout a] -> Layout a
-rightToLeftContainer x cs = unsafePerformIO $ do
-  ptr <- c'YGNodeNew
-  c'YGNodeStyleSetDirection ptr c'YGDirectionRTL
-  addChildrenToContainer ptr x cs
+  foldl f z (Container x cs _) = foldl (foldl f) (f z x) cs
+  foldl f z (Leaf x _) = f z x
+
+  foldr f z (Container x cs _) = f x $ foldr (flip $ foldr f) z cs
+  foldr f z (Leaf x _) = f x z
+
+instance Traversable Layout where
+  traverse f (Container x cs ptr) =
+    Container <$> f x <*> (sequenceA $ traverse f <$> cs) <*> pure ptr
+  traverse f (Leaf x ptr) = Leaf <$> f x <*> pure ptr
+
+  sequenceA (Container x cs ptr) =
+    Container <$> x <*> sequenceA (sequenceA <$> cs) <*> pure ptr
+  sequenceA (Leaf x ptr) = Leaf <$> x <*> pure ptr
 
 calculateLayout :: Monad m => NativeNode -> m ()
 calculateLayout ptr = do
@@ -133,29 +107,248 @@ layoutBounds ptr =
     getLayout lytFn = cf $ unsafePerformIO $ withForeignPtr ptr lytFn
 
 renderTree :: Monad m => Layout a -> RenderFn m a b -> m (Layout b)
-renderTree (Layout x cs ptr) f = do
+renderTree (Leaf x ptr) f = do
   (left, top, width, height) <- return $ layoutBounds ptr
-  Layout
+  Leaf <$> f (left, top) (width, height) x <*> pure ptr
+renderTree (Container x cs ptr) f = do
+  (left, top, width, height) <- return $ layoutBounds ptr
+  Container
     <$> f (left, top) (width, height) x
     <*> mapM (flip renderTree f) cs
     <*> pure ptr
 
 render :: Monad m => Layout a -> RenderFn m a b -> m (Layout b)
-render lyt@(Layout _ _ ptr) f = do
-  _ <- calculateLayout ptr
+render lyt f = do
+  _ <- calculateLayout (internalPtr lyt)
   renderTree lyt f
 
 foldRenderTree :: (Monad m, Monoid b) =>
                   Layout a -> RenderFn m a (b, c) -> m (b, Layout c)
-foldRenderTree (Layout x cs ptr) f = do
+foldRenderTree (Leaf x ptr) f = do
+  (left, top, width, height) <- return $ layoutBounds ptr
+  (m, y) <- f (left, top) (width, height) x
+  return (mappend m mempty, Leaf y ptr)
+foldRenderTree (Container x cs ptr) f = do
   (left, top, width, height) <- return $ layoutBounds ptr
   (m, y) <- f (left, top) (width, height) x
   cs' <- mapM (flip foldRenderTree f) cs
   return (mappend m . foldr mappend mempty . map fst $ cs',
-          Layout y (map snd cs') ptr)
+          Container y (map snd cs') ptr)
 
 foldRender :: (Monad m, Monoid b) =>
               Layout a -> RenderFn m a (b, c) -> m (b, Layout c)
-foldRender lyt@(Layout _ _ ptr) f = do
-  _ <- calculateLayout ptr
+foldRender lyt f = do
+  _ <- calculateLayout (internalPtr lyt)
   foldRenderTree lyt f
+
+startToEnd :: [Layout a] -> Children a
+startToEnd = StartToEnd
+
+endToStart :: [Layout a] -> Children a
+endToStart = EndToStart
+
+centered :: [Layout a] -> Children a
+centered = Centered
+
+spaceBetween :: [Layout a] -> Children a
+spaceBetween = SpaceBetween
+
+spaceAround :: [Layout a] -> Children a
+spaceAround = SpaceAround
+
+wrapped :: Children a -> Children a
+wrapped (Wrap xs) = Wrap xs
+wrapped childs = childs
+
+justifiedContainer :: CInt -> [Layout a] -> a -> IO (Layout a)
+justifiedContainer just cs x = do
+  ptr <- c'YGNodeNew
+  c'YGNodeStyleSetJustifyContent ptr just
+  c'YGNodeStyleSetFlexWrap ptr c'YGWrapNoWrap
+
+  forM_ (zip cs [0..]) $ \(child, idx) ->
+    withForeignPtr (internalPtr child) $ \childPtr ->
+    c'YGNodeInsertChild ptr childPtr idx
+  Container x cs <$> newForeignPtr p'YGNodeFree ptr
+
+assembleChildren :: Children a -> a -> IO (Layout a)
+assembleChildren (StartToEnd cs) x = justifiedContainer c'YGJustifyFlexStart cs x
+assembleChildren (EndToStart cs) x = justifiedContainer c'YGJustifyFlexEnd cs x
+assembleChildren (Centered cs) x = justifiedContainer c'YGJustifyCenter cs x
+assembleChildren (SpaceBetween cs) x = justifiedContainer c'YGJustifySpaceBetween cs x
+assembleChildren (SpaceAround cs) x = justifiedContainer c'YGJustifySpaceAround cs x
+assembleChildren (Wrap cs) x = assembleChildren cs x >>= wrapContainer
+  where
+    wrapContainer :: Layout a -> IO (Layout a)
+    wrapContainer lyt = do
+      withForeignPtr (internalPtr lyt) $ \ptr ->
+        c'YGNodeStyleSetFlexWrap ptr c'YGWrapWrap
+      return lyt
+
+setContainerDirection :: CInt -> CInt -> Layout a -> IO ()
+setContainerDirection dir flexDir lyt =
+  withForeignPtr (internalPtr lyt) $ \ptr -> do
+    c'YGNodeStyleSetDirection ptr dir
+    c'YGNodeStyleSetFlexDirection ptr flexDir
+
+hbox :: Children a -> a -> Layout a
+hbox cs x = unsafePerformIO $ do
+  node <- assembleChildren cs x
+  setContainerDirection c'YGDirectionInherit c'YGFlexDirectionRow node
+  return node
+
+vbox :: Children a -> a -> Layout a
+vbox cs x = unsafePerformIO $ do
+  node <- assembleChildren cs x
+  setContainerDirection c'YGDirectionInherit c'YGFlexDirectionColumn node
+  return node
+
+hboxLeftToRight :: Children a -> a -> Layout a
+hboxLeftToRight cs x = unsafePerformIO $ do
+  node <- assembleChildren cs x
+  setContainerDirection c'YGDirectionLTR c'YGFlexDirectionRow node
+  return node
+
+hboxRightToLeft :: Children a -> a -> Layout a
+hboxRightToLeft cs x = unsafePerformIO $ do
+  node <- assembleChildren cs x
+  setContainerDirection c'YGDirectionRTL c'YGFlexDirectionRow node
+  return node
+
+vboxTopToBottom :: Children a -> a -> Layout a
+vboxTopToBottom cs x = unsafePerformIO $ do
+  node <- assembleChildren cs x
+  setContainerDirection c'YGDirectionLTR c'YGFlexDirectionColumn node
+  return node
+
+vboxBottomToTop :: Children a -> a -> Layout a
+vboxBottomToTop cs x = unsafePerformIO $ do
+  node <- assembleChildren cs x
+  setContainerDirection c'YGDirectionRTL c'YGFlexDirectionColumn node
+  return node
+
+data Size
+  = Exact Float
+  | Min Float
+  | Max Float
+  | Range Float Float
+
+mkNode :: a -> IO (Layout a)
+mkNode x = do
+  ptr <- c'YGNodeNew
+  Leaf x <$> newForeignPtr p'YGNodeFree ptr
+  
+setWidth :: Size -> Layout a -> IO ()
+setWidth (Exact w) lyt =
+  withForeignPtr (internalPtr lyt) $ \ptr ->
+    c'YGNodeStyleSetWidth ptr $ realToFrac w
+setWidth (Min w) lyt =
+  withForeignPtr (internalPtr lyt) $ \ptr ->
+    c'YGNodeStyleSetMinWidth ptr $ realToFrac w
+setWidth (Max w) lyt =
+  withForeignPtr (internalPtr lyt) $ \ptr ->
+    c'YGNodeStyleSetMaxWidth ptr $ realToFrac w
+setWidth (Range minWidth maxWidth) lyt =
+  withForeignPtr (internalPtr lyt) $ \ptr -> do
+    c'YGNodeStyleSetMinWidth ptr $ realToFrac minWidth
+    c'YGNodeStyleSetMaxWidth ptr $ realToFrac maxWidth
+
+setHeight :: Size -> Layout a -> IO ()
+setHeight (Exact h) lyt =
+  withForeignPtr (internalPtr lyt) $ \ptr ->
+    c'YGNodeStyleSetHeight ptr $ realToFrac h
+setHeight (Min h) lyt =
+  withForeignPtr (internalPtr lyt) $ \ptr ->
+    c'YGNodeStyleSetMinHeight ptr $ realToFrac h
+setHeight (Max h) lyt =
+  withForeignPtr (internalPtr lyt) $ \ptr ->
+    c'YGNodeStyleSetMaxHeight ptr $ realToFrac h
+setHeight (Range minHeight maxHeight) lyt =
+  withForeignPtr (internalPtr lyt) $ \ptr -> do
+    c'YGNodeStyleSetMinHeight ptr $ realToFrac minHeight
+    c'YGNodeStyleSetMaxHeight ptr $ realToFrac maxHeight
+
+shrinkable :: Float -> Size -> Size -> a -> Layout a
+shrinkable weight width height x = unsafePerformIO $ do
+  n <- mkNode x
+  setWidth width n
+  setHeight height n
+  withForeignPtr (internalPtr n) $ \ptr ->
+    c'YGNodeStyleSetFlexShrink ptr $ realToFrac weight
+  return n
+
+growable :: Float -> Size -> Size -> a -> Layout a
+growable weight width height x = unsafePerformIO $ do
+  n <- mkNode x
+  setWidth width n
+  setHeight height n
+  withForeignPtr (internalPtr n) $ \ptr ->
+    c'YGNodeStyleSetFlexGrow ptr $ realToFrac weight
+  return n
+
+exact :: Float -> Float -> a -> Layout a
+exact width height x = unsafePerformIO $ do
+  n <- mkNode x
+  setWidth (Exact width) n
+  setHeight (Exact height) n
+  return n
+
+stretched :: (a -> Layout a) -> a -> Layout a
+stretched mkNodeFn x =
+  let node = mkNodeFn x
+  in unsafePerformIO $ do
+    withForeignPtr (internalPtr node) $ \ptr ->
+      c'YGNodeStyleSetAlignSelf ptr c'YGAlignStretch
+    return node
+
+setMargin :: CInt -> Float -> Layout a -> IO (Layout a)
+setMargin edge px node = do
+  withForeignPtr (internalPtr node) $ \ptr ->
+    c'YGNodeStyleSetMargin ptr edge $ realToFrac px
+  return node
+
+marginSetWith :: Edge -> Float -> (a -> Layout a) -> a -> Layout a
+marginSetWith Edge'Left px mkNodeFn x =
+  unsafePerformIO $ setMargin c'YGEdgeLeft px (mkNodeFn x)
+marginSetWith Edge'Top px mkNodeFn x =
+  unsafePerformIO $ setMargin c'YGEdgeTop px (mkNodeFn x)
+marginSetWith Edge'Right px mkNodeFn x =
+  unsafePerformIO $ setMargin c'YGEdgeRight px (mkNodeFn x)
+marginSetWith Edge'Bottom px mkNodeFn x =
+  unsafePerformIO $ setMargin c'YGEdgeBottom px (mkNodeFn x)
+marginSetWith Edge'Start px mkNodeFn x =
+  unsafePerformIO $ setMargin c'YGEdgeStart px (mkNodeFn x)
+marginSetWith Edge'End px mkNodeFn x =
+  unsafePerformIO $ setMargin c'YGEdgeEnd px (mkNodeFn x)
+marginSetWith Edge'Horizontal px mkNodeFn x =
+  unsafePerformIO $ setMargin c'YGEdgeHorizontal px (mkNodeFn x)
+marginSetWith Edge'Vertical px mkNodeFn x =
+  unsafePerformIO $ setMargin c'YGEdgeVertical px (mkNodeFn x)
+marginSetWith Edge'All px mkNodeFn x =
+  unsafePerformIO $ setMargin c'YGEdgeAll px (mkNodeFn x)
+
+setPadding :: CInt -> Float -> Layout a -> IO (Layout a)
+setPadding edge px node = do
+  withForeignPtr (internalPtr node) $ \ptr ->
+    c'YGNodeStyleSetPadding ptr edge $ realToFrac px
+  return node
+
+paddingSetWith :: Edge -> Float -> (a -> Layout a) -> a -> Layout a
+paddingSetWith Edge'Left px mkNodeFn x =
+  unsafePerformIO $ setPadding c'YGEdgeLeft px (mkNodeFn x)
+paddingSetWith Edge'Top px mkNodeFn x =
+  unsafePerformIO $ setPadding c'YGEdgeTop px (mkNodeFn x)
+paddingSetWith Edge'Right px mkNodeFn x =
+  unsafePerformIO $ setPadding c'YGEdgeRight px (mkNodeFn x)
+paddingSetWith Edge'Bottom px mkNodeFn x =
+  unsafePerformIO $ setPadding c'YGEdgeBottom px (mkNodeFn x)
+paddingSetWith Edge'Start px mkNodeFn x =
+  unsafePerformIO $ setPadding c'YGEdgeStart px (mkNodeFn x)
+paddingSetWith Edge'End px mkNodeFn x =
+  unsafePerformIO $ setPadding c'YGEdgeEnd px (mkNodeFn x)
+paddingSetWith Edge'Horizontal px mkNodeFn x =
+  unsafePerformIO $ setPadding c'YGEdgeHorizontal px (mkNodeFn x)
+paddingSetWith Edge'Vertical px mkNodeFn x =
+  unsafePerformIO $ setPadding c'YGEdgeVertical px (mkNodeFn x)
+paddingSetWith Edge'All px mkNodeFn x =
+  unsafePerformIO $ setPadding c'YGEdgeAll px (mkNodeFn x)
